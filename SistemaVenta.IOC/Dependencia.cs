530fcd073c0251db07ef235ba6179ff76473abee
DavidRipFernandez <74621200+DavidRipFernandez@users.noreply.github.com>
2023-10-23 14:34:30 -0400

    Configuracion inicial del proyecto

---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SistemaVentas.DAL.Implementation;
+//using SistemaVentas.DAL.Interfaces;
+//using SistemaVenta.BLL.Interfaces;
+//using SistemaVenta.BLL.Implementation;
+
+// se agregaran todas las dependecias que se usaran en este clase
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        //todos los servicios iran mas adelante en este lado
+        public static void InyectarDependecia(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<NsoftContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CadenaSQL"));
+            }
+            );
+        }
+    }
+}
