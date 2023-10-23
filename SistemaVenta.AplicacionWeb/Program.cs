ede87cdfd9799e608a9306628ed0b291cfbc5fbd
DavidRipFernandez <74621200+DavidRipFernandez@users.noreply.github.com>
2023-10-23 17:55:00 -0400

    Primeras configuraciones en el proyecto PosMinerva

--- SistemaVenta.AplicacionWeb/Program.cs
+++ SistemaVenta.AplicacionWeb/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+// connection for database sql server
 builder.Services.InyectarDependecia(builder.Configuration);
 
 
