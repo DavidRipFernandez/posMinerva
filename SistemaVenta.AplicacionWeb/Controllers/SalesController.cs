4e7ecbd34255da54f8b703b4ed576122b935dc62
DavidRipFernandez <74621200+DavidRipFernandez@users.noreply.github.com>
2023-10-24 23:12:53 -0400

    Se ha trabajado en la vista de nueva venta y historial de venta

---
+++ SistemaVenta.AplicacionWeb/Controllers/SalesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult NewSales()
+        {
+            return View();
+        }
+
+        public IActionResult SalesHistory()
+        {
+            return View();
+        }
+    }
+}
